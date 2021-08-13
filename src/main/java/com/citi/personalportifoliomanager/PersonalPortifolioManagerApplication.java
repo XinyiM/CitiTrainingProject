a4f0356e1308446f083c44c4c2a825eac4bdef26
XinyiM <xinyima0625@gmail.com>
2021-08-12 17:13:27 -0700

    First Commit

---
+++ src/main/java/com/citi/personalportifoliomanager/PersonalPortifolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.citi.personalportifoliomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortifolioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPortifolioManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citi/personalportifoliomanager/PersonalPortifolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.personalportifoliomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPortifolioManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
