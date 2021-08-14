2957f2e8369a8e2d50284036762e66eda6f4792b
XinyiM <xinyima0625@gmail.com>
2021-08-14 14:52:02 -0700

    updated cash amount when adding cash transactions

--- src/main/java/com/citi/personalportifoliomanager/service/CashTransactionServiceImpl.java
+++ src/main/java/com/citi/personalportifoliomanager/service/CashTransactionServiceImpl.java
@@ -2,18 +2,21 @@ package com.citi.personalportifoliomanager.service;
 
 import com.citi.personalportifoliomanager.entities.Cash;
 import com.citi.personalportifoliomanager.entities.CashTransaction;
-import com.citi.personalportifoliomanager.entities.User;
+import com.citi.personalportifoliomanager.repos.CashRepository;
 import com.citi.personalportifoliomanager.repos.CashTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.*;
-import java.util.*;
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class CashTransactionServiceImpl implements CashTransactionService{
     @Autowired
     private CashTransactionRepository cashTransactionRepository;
+
+    @Autowired
+    private CashRepository cashRepository;
 
     @Override
     public Collection<CashTransaction> findCashTransactionByCashId(int cashId) {
@@ -23,6 +26,10 @@ public class CashTransactionServiceImpl implements CashTransactionService{
     @Override
     public CashTransaction saveCashTransaction(CashTransaction cashTransaction) {
         System.out.println(cashTransaction.toString());
+        int cashId = cashTransaction.getCashId();
+        Cash cash = cashRepository.getById(cashId);
+        cash.setAmount(cash.getAmount() + cashTransaction.getAmount());
+        cashRepository.save(cash);
         return cashTransactionRepository.save(cashTransaction);
     }
 
